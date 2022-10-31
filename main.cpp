c48f56e435327597c01375a3ad1b08ceabd17a38
Alberto Nidasio <alberto.nidasio@skywarder.eu>
2022-10-31 15:11:52 +0100

    Tested DAC wave generator

--- main.cpp
+++ main.cpp
@@ -4,20 +4,37 @@
 using namespace std;
 using namespace miosix;
 
+constexpr long long DEMO_DURATION = 5 * 1000;  // [ms]
+
+// Serial pins: PA9 PA10
+
 int main() {
     DACDriver adc;
+    long long start;
 
     adc.enableChannel(1);
 
-    float voltage = 0;
-
     while (true) {
-        adc.setChannel(voltage, 1);
+        // Noise generator
+        adc.enableNoiseGeneration(1);
+        adc.setWaveGeneratorMask(1, 10);
+        adc.centerWaveOutput(1, 1);
+        start = getTick();
+        while (getTick() - start < DEMO_DURATION) {
+            adc.dispatchSoftwareTrigger(1);
+            Thread::sleep(10);
+        }
+        adc.disableWaveGenerator(1);
 
-        voltage += 0.5;
-        if (voltage > 3)
-            voltage = 0;
-
-        delayMs(1);
+        // Triangular wave generator
+        adc.enableTriangularWaveGeneration(1);
+        adc.setWaveGeneratorMask(1, 10);
+        adc.centerWaveOutput(1, 2);
+        start = getTick();
+        while (getTick() - start < DEMO_DURATION) {
+            adc.dispatchSoftwareTrigger(1);
+            delayUs(100);
+        }
+        adc.disableWaveGenerator(1);
     }
 }
