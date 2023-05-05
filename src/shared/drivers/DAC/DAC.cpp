c459d2de679c26a963b40e842ee24932a3f11aca
Alberto Nidasio <alberto.nidasio@skywarder.eu>
2023-05-05 21:05:11 +0200

    Added DAC driver

---
+++ src/shared/drivers/DAC/DAC.cpp
@@ -0,0 +1,211 @@
+/***************************************************************************
+ *   Copyright (C) 2022 by Alberto Nidasio                                 *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   As a special exception, if other files instantiate templates or use   *
+ *   macros or inline functions from this file, or you compile this file   *
+ *   and link it with other works to produce a work based on this file,    *
+ *   this file does not by itself cause the resulting work to be covered   *
+ *   by the GNU General Public License. However the source code for this   *
+ *   file must still be made available in accordance with the GNU General  *
+ *   Public License. This exception does not invalidate any other reasons  *
+ *   why a work based on this file might be covered by the GNU General     *
+ *   Public License.                                                       *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, see <http://www.gnu.org/licenses/>   *
+ ***************************************************************************/
+
+#include "DAC.h"
+
+#include <interfaces/gpio.h>
+#include <kernel/kernel.h>
+#include <math.h>
+
+#ifndef V_DDA_VOLTAGE
+#error "V_DDA_VOLTAGE not defined. Pease specify the analog supply voltage."
+#endif
+
+using namespace miosix;
+
+typedef Gpio<GPIOA_BASE, 4> ch1;
+typedef Gpio<GPIOA_BASE, 5> ch2;
+
+void DACDriver::enableChannel(Channel channel) {
+    if (channel == Channel::CH1)
+        ch1::mode(Mode::INPUT_ANALOG);
+    else
+        ch2::mode(Mode::INPUT_ANALOG);
+
+    // DAC clock must be enabled
+    RCC->APB1ENR |= RCC_APB1ENR_DACEN;
+
+    // Enable the channel
+    DAC->CR |= channel == Channel::CH1 ? DAC_CR_EN1 : DAC_CR_EN2;
+}
+
+void DACDriver::disableChannel(Channel channel) {
+    // Leave the gpio pins in analog configuration to consume less power.
+    // Check AN4899 chapter 7
+
+    // Disable the DAC clock if both channels are disabled
+    if (!(DAC->CR & (channel == Channel::CH1 ? DAC_CR_EN2 : DAC_CR_EN1)))
+        RCC->APB1ENR &= ~RCC_APB1ENR_DACEN;
+}
+
+void DACDriver::setChannel(Channel channel, float voltage) {
+    if (voltage > V_DDA_VOLTAGE)
+        voltage = V_DDA_VOLTAGE;
+
+    if (channel == Channel::CH1)
+        DAC->DHR12R1 = static_cast<uint16_t>(0xfff / V_DDA_VOLTAGE * voltage);
+    else
+        DAC->DHR12R2 = static_cast<uint16_t>(0xfff / V_DDA_VOLTAGE * voltage);
+}
+
+void DACDriver::enableBuffer(Channel channel) {
+    if (channel == Channel::CH1)
+        DAC->CR |= DAC_CR_BOFF1;
+    else
+        DAC->CR |= DAC_CR_BOFF2;
+}
+
+void DACDriver::disableBuffer(Channel channel) {
+    if (channel == Channel::CH1)
+        DAC->CR &= ~DAC_CR_BOFF1;
+    else
+        DAC->CR &= ~DAC_CR_BOFF2;
+}
+
+void DACDriver::enableTrigger(Channel channel, TriggerSource source) {
+    // Prevent other threads or interrupts to access the DAC registers while
+    // configuring
+    FastInterruptDisableLock l;
+
+    // We need to:
+    // - First disable the trigger before changing the TSEL bits
+    // - Change the trigger source (TSEL bits)
+    // - Enable the trigger source
+
+    if (channel == Channel::CH1) {
+        DAC->CR &= ~DAC_CR_TEN1;
+        DAC->CR &= ~DAC_CR_TSEL1;
+        DAC->CR |= static_cast<uint32_t>(source);
+        DAC->CR |= DAC_CR_TEN1;
+    } else {
+        DAC->CR &= ~DAC_CR_TEN2;
+        DAC->CR &= ~DAC_CR_TSEL2;
+        DAC->CR |= static_cast<uint32_t>(source) << 16;
+        DAC->CR |= DAC_CR_TEN2;
+    }
+}
+
+void DACDriver::disableTrigger(Channel channel) {
+    if (channel == Channel::CH1)
+        DAC->CR &= ~DAC_CR_TEN1;
+    else
+        DAC->CR &= ~DAC_CR_TEN2;
+}
+
+void DACDriver::dispatchSoftwareTrigger(Channel channel) {
+    // The SWTRIGRx bit will be reset by hardware once the current DHRx value is
+    // shifted into DORx
+    if (channel == Channel::CH1)
+        DAC->SWTRIGR |= DAC_SWTRIGR_SWTRIG1;
+    else
+        DAC->SWTRIGR |= DAC_SWTRIGR_SWTRIG2;
+}
+
+void DACDriver::enableNoiseGeneration(Channel channel, TriggerSource source) {
+    // The noise generator requires a trigger source, so we'll enable it
+    enableTrigger(channel, source);
+
+    // Enable the noise generator and set the mask to unmask all bits by default
+    if (channel == Channel::CH1) {
+        DAC->CR &= ~DAC_CR_WAVE1;
+        DAC->CR |= DAC_CR_WAVE1_0 | DAC_CR_MAMP1;
+    } else {
+        DAC->CR &= ~DAC_CR_WAVE2;
+        DAC->CR |= DAC_CR_WAVE2_0 | DAC_CR_MAMP2;
+    }
+}
+
+void DACDriver::enableTriangularWaveGeneration(Channel channel,
+                                               TriggerSource source) {
+    // The noise generator requires a trigger source, so we'll enable it
+    enableTrigger(channel, source);
+
+    // Enable the noise generator and set the mask to full amplitude by default
+    if (channel == Channel::CH1) {
+        DAC->CR &= ~DAC_CR_WAVE1;
+        DAC->CR |= DAC_CR_WAVE1_1 | DAC_CR_MAMP1;
+    } else {
+        DAC->CR &= ~DAC_CR_WAVE2;
+        DAC->CR |= DAC_CR_WAVE2_1 | DAC_CR_MAMP2;
+    }
+}
+
+void DACDriver::disableWaveGenerator(Channel channel) {
+    // Disable the trigger that was enabled
+    disableTrigger(channel);
+
+    // Disable the wave generator
+    if (channel == Channel::CH1)
+        DAC->CR &= ~DAC_CR_WAVE1;
+    else
+        DAC->CR &= ~DAC_CR_WAVE2;
+}
+
+bool DACDriver::setWaveGeneratorMask(Channel channel, uint8_t bits) {
+    if (bits < 1 || bits > 12)
+        return false;
+
+    if (channel == Channel::CH1) {
+        DAC->CR &= ~DAC_CR_MAMP1;    // Reset the value
+        DAC->CR |= (bits - 1) << 8;  // Set the new value
+    } else {
+        DAC->CR &= ~DAC_CR_MAMP2;     // Reset the value
+        DAC->CR |= (bits - 1) << 24;  // Set the new value
+    }
+
+    return true;
+}
+
+void DACDriver::centerWaveOutput(Channel channel, float voltage) {
+    uint8_t bits;
+
+    // Fetch the unmasked bits
+    if (channel == Channel::CH1)
+        bits = ((DAC->CR & DAC_CR_MAMP1) >> 8) + 1;
+    else
+        bits = ((DAC->CR & DAC_CR_MAMP2) >> 24) + 1;
+
+    // Compute the wave amplitude
+    float amplitude = V_DDA_VOLTAGE / 4095.0 * (powf(2, bits) - 1);
+
+    // Set the appropriate voltage
+    setChannel(channel, voltage - amplitude / 2);
+}
+
+void DACDriver::enableDMA(Channel channel) {
+    if (channel == Channel::CH1)
+        DAC->CR |= DAC_CR_DMAEN1;
+    else
+        DAC->CR |= DAC_CR_DMAEN2;
+}
+
+void DACDriver::disableDMA(Channel channel) {
+    if (channel == Channel::CH1)
+        DAC->CR &= ~DAC_CR_DMAEN1;
+    else
+        DAC->CR &= ~DAC_CR_DMAEN2;
+}
---
+++ src/shared/drivers/DAC/DAC.h
@@ -0,0 +1,240 @@
+/***************************************************************************
+ *   Copyright (C) 2022 by Alberto Nidasio                                 *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   As a special exception, if other files instantiate templates or use   *
+ *   macros or inline functions from this file, or you compile this file   *
+ *   and link it with other works to produce a work based on this file,    *
+ *   this file does not by itself cause the resulting work to be covered   *
+ *   by the GNU General Public License. However the source code for this   *
+ *   file must still be made available in accordance with the GNU General  *
+ *   Public License. This exception does not invalidate any other reasons  *
+ *   why a work based on this file might be covered by the GNU General     *
+ *   Public License.                                                       *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, see <http://www.gnu.org/licenses/>   *
+ ***************************************************************************/
+
+#pragma once
+
+#include <interfaces/arch_registers.h>
+
+/**
+ * Driver for the DAC peripheral in STM32F4 microcontrollers.
+ */
+class DACDriver {
+public:
+    enum class Channel {
+        CH1,
+        CH2,
+    };
+
+    enum class TriggerSource : uint32_t {
+        TIM6_TRGO = 0,
+        TIM3_TRGO = DAC_CR_TSEL1_0,
+        TIM7_TRGO = DAC_CR_TSEL1_1,
+        TIM15_TRGO = DAC_CR_TSEL1_1 | DAC_CR_TSEL1_0,
+        TIM2_TRGO = DAC_CR_TSEL1_2,
+        EXTI_line_9 = DAC_CR_TSEL1_2 | DAC_CR_TSEL1_1,
+        SWTRIG = DAC_CR_TSEL1
+    };
+
+    /**
+     * @brief Enables one channel of the DAC.
+     *
+     * @param channel Channel number, must be either 1 or 2.
+     * @return True if the channel was enabled.
+     */
+    void enableChannel(Channel channel);
+
+    /**
+     * @brief Disables one channel of the DAC.
+     *
+     * @param channel Channel number, must be either 1 or 2.
+     * @return True if the channel was disabled.
+     */
+    void disableChannel(Channel channel);
+
+    /**
+     * @brief Set the channel output voltage
+     *
+     * @param voltage On most boards the output is between 0V and 3V
+     * @param channel Channel number, must be either 1 or 2.
+     * @return True if the output voltage was changed.
+     */
+    void setChannel(Channel channel, float voltage);
+
+    /**
+     * @brief Enables the internal output buffer.
+     *
+     * @param channel Channel number, must be either 1 or 2.
+     * @return True if the output buffer was enabled.
+     */
+    void enableBuffer(Channel channel);
+
+    /**
+     * @brief Disables the internal output buffer.
+     *
+     * @param channel Channel number, must be either 1 or 2.
+     * @return True if the output buffer was disabled.
+     */
+    void disableBuffer(Channel channel);
+
+    /**
+     * @brief Enables the external trigger.
+     *
+     * This function allows to set and change the trigger source. It first
+     * disables the trigger, changes the source and then re-enables it.
+     *
+     * @param channel Channel number, must be either 1 or 2.
+     * @param source Trigger source to be set.
+     * @return True if the output buffer was disabled.
+     */
+    void enableTrigger(Channel channel,
+                       TriggerSource source = TriggerSource::SWTRIG);
+
+    /**
+     * @brief Disables the external trigger.
+     *
+     * @param channel Channel number, must be either 1 or 2.
+     * @return True if the output buffer was disabled.
+     */
+    void disableTrigger(Channel channel);
+
+    /**
+     * @brief Triggers the software trigger.
+     *
+     * The new DAC conversion starts one APB clock cycle after the trigger.
+     *
+     * @param channel Channel number, must be either 1 or 2.
+     * @return True if the output buffer was disabled.
+     */
+    void dispatchSoftwareTrigger(Channel channel);
+
+    /**
+     * @brief Enables the noise generator.
+     *
+     * The noise generator allow to create a variable amplitude pseudo noise.
+     * The noise generator requires a trigger source.
+     *
+     * At each trigger event the wave generator output value is updated and then
+     * added to the DHRx register (the normal DAC output). This allows to offset
+     * the wave by a specific amount.
+     *
+     * The wave generator output can also be masked to control the wave
+     * amplitude. This allows to control the amplitude in step of 2^-n.
+     *
+     * @warning If you see the wave with an offset then use setChannel(x, 0) to
+     * zero the offset.
+     *
+     * By default this function enables the software trigger source.
+     * @see DACDriver::enableTrigger to change the trigger source.
+     * @see DACDriver::dispatchSoftwareTrigger to trigger an event.
+     *
+     * @param channel Channel number, must be either 1 or 2.
+     * @param source Trigger source to be set. Software trigger by default.
+     * @return True if the output buffer was disabled.
+     */
+    void enableNoiseGeneration(Channel channel,
+                               TriggerSource source = TriggerSource::SWTRIG);
+
+    /**
+     * @brief Enables the triangular wave generator.
+     *
+     * The triangular wave generator allow to create a variable amplitude wave.
+     * The generator requires a trigger source.
+     *
+     * At each trigger event the wave generator output value is updated and then
+     * added to the DHRx register (the normal DAC output). This allows to offset
+     * the wave by a specific amount.
+     *
+     * The wave generator output can also be masked to control the wave
+     * amplitude. This allows to control the amplitude in step of 2^-n. @see
+     * DACDriver::setWaveGeneratorMask for more informations.
+     *
+     * @warning If you see the wave with an offset then use setChannel(x, 0) to
+     * zero the offset.
+     *
+     * By default this function enables the software trigger source.
+     * @see DACDriver::enableTrigger to change the trigger source.
+     * @see DACDriver::dispatchSoftwareTrigger to trigger an event.
+     *
+     * @param channel Channel number, must be either 1 or 2.
+     * @param source Trigger source to be set. Software trigger by default.
+     * @return True if the output buffer was disabled.
+     */
+    void enableTriangularWaveGeneration(
+        Channel channel, TriggerSource source = TriggerSource::SWTRIG);
+
+    /**
+     * @brief Disables the wave generator.
+     *
+     * @warning Also disables the trigger source.
+     *
+     * @param channel Channel number, must be either 1 or 2.
+     * @return True if the output buffer was disabled.
+     */
+    void disableWaveGenerator(Channel channel);
+
+    /**
+     * @brief Allows to configure the wave amplitude.
+     *
+     * The wave generator can be configured to mask specific bits of the
+     * generated output. This allows to control the amplitude in step of 2^-n:
+     * - bits = 12: All bits are masked -> Amplitude = V_DDA
+     * - bits = 11: 11 bits masked -> Amplitude = V_DDA / 2
+     * - ...
+     * - bits =  1: Only the first bit is masked -> Amplitude = V_DDA / 4096
+     *
+     * @param channel Channel number, must be either 1 or 2.
+     * @param bits Bits to mask, must be between 1 and 12.
+     * @return True if the output buffer was disabled.
+     */
+    bool setWaveGeneratorMask(Channel channel, uint8_t bits);
+
+    /**
+     * @brief Utility function that sets the channel voltage to center the wave.
+     *
+     * Since the wave generator output is added to the channel configured value,
+     * this function uses the currently configured wave amplitude to calculate
+     * the proper channel output such that the wave is centered on a specific
+     * voltage value.
+     *
+     * @warning If you measure the wave non correctly center, measure your V_DDA
+     * actual value.
+     *
+     * @param channel Channel number, must be either 1 or 2.
+     * @param voltage Voltage value to center the wave output.
+     * @return True if the output buffer was disabled.
+     */
+    void centerWaveOutput(Channel channel, float voltage);
+
+    /**
+     * @brief Enables DMA on the specified channel.
+     *
+     * When the DMA is enabled, a new DMA request is generated when an external
+     * trigger (but not a software trigger) occurs.
+     *
+     * @param channel Channel number, must be either 1 or 2.
+     * @return True if the output buffer was enabled.
+     */
+    void enableDMA(Channel channel);
+
+    /**
+     * @brief Disables DMA on the specified channel.
+     *
+     * @param channel Channel number, must be either 1 or 2.
+     * @return True if the output buffer was enabled.
+     */
+    void disableDMA(Channel channel);
+};
